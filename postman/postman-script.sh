b3790785c8a981c41fd5030950469a2945f38b72
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2021-06-25 15:16:50 -0300

    github actions

--- postman/postman-script.sh
+++ postman/postman-script.sh
@@ -1,1 +1,1 @@
-newman run postman/Compound\ Interest.postman_collection.json -d tests/data-tests.json -r htmlextra
+newman run postman/compound_interest.postman_collection.json -d tests/data-tests.json -r htmlextra
